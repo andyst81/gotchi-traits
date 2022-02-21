6e303c9088df8650e04340634d76f4a18fd76cef
andy <andyst81@users.noreply.github.com>
2022-02-21 20:08:24 +0700

    change next js to create react app

---
+++ front-end/src/components/WalletForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Gotchis from './PullGotchis.js'
+import { useState } from 'react';
+
+
+export function WalletForm() {
+  const [address, setAddress] = useState("")
+  const [traits, setTraits] = useState('baseStats')
+
+
+  return (
+    <div>
+      <form className="w-full max-w-md py-6">
+      <div className="flex items-center border-b border-blue-700 min-w-full">
+        <input className="appearance-none bg-transparent border-none min-w-full text-blue-700" 
+          type="text" 
+          placeholder="Wallet Address" 
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+        />
+      </div>
+    </form>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded-full"
+      onClick={() => setTraits('baseStats')}>
+        Base Stats
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded-full"
+      onClick={() => setTraits('modifiedStats')}>
+        With Wearables
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded-full"
+      onClick={() => setTraits('withSetStats')}>
+        With Bonuses
+      </button>
+    { address ? <Gotchis address={address} traits={traits}/> : null }
+
+  </div>
+  );
+}
+
+
+export default WalletForm
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,34 +1,9 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    theme: {
-      extend: {
-        lineHeight: {
-          3: '1.2rem',
-          4: '1.6rem',
-          5: '2.0rem',
-          6: '2.4rem',
-          7: '2.8rem',
-          8: '3.2rem',
-          9: '3.6rem',
-          10: '4.0rem',
-        },
-        fontSize: {
-          xs: ['1.2rem', { lineHeight: '1.6rem' }],
-          sm: ['1.4rem', { lineHeight: '2.0rem' }],
-          base: ['1.6rem', { lineHeight: '2.4rem' }],
-          lg: ['1.8rem', { lineHeight: '2.8rem' }],
-          xl: ['2.0rem', { lineHeight: '2.8rem' }],
-          '2xl': ['2.4rem', { lineHeight: '3.2rem' }],
-          '3xl': ['3.0rem', { lineHeight: '3.6rem' }],
-          '4xl': ['3.6rem', { lineHeight: '4.0rem' }],
-          '5xl': ['4.8rem', { lineHeight: '1' }]
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
